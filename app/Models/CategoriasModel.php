0ecd81ba7c625f3f2bf21d180eb2298a564e9742
Carlos Daniel Campos Castro <dannybv7@hotmail.com>
2024-07-03 22:52:41 -0700

    Creacion de modelo y controlador de categorias

---
+++ app/Models/CategoriasModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CategoriasModel extends Model
+{
+    protected $table            = 'categorias';
+    protected $primaryKey       = 'cat_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['cat_nombre'];
+
+    // Dates
+    protected $useTimestamps = false;
+
+}
