9620c3d882a8584f3116575ed95e5224126f6680
Carlos Daniel Campos Castro <dannybv7@hotmail.com>
2024-07-03 18:35:05 -0700

    configuracion de la ruta base y la conexion a la base de datos

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'pruebatecnica_digiosoft',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
