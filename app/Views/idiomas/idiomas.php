1556fc9b49d4a7fbfb5e073ce6872f1114d2873b
Carlos Daniel Campos Castro <dannybv7@hotmail.com>
2024-07-06 02:13:46 -0700

    Agregado el navbar a todas las ventanas principales

--- app/Views/idiomas/idiomas.php
+++ app/Views/idiomas/idiomas.php
@@ -6,6 +6,43 @@
     <title>Idiomas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?= base_url('/') ?>">Inicio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('libros') ?>">Libros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('prestamos') ?>">Prestamos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('ejemplares') ?>">Ejemplares</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('categorias') ?>">Categorias</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('autores') ?>">Autores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('editorial') ?>">Editoriales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('idiomas') ?>">Idiomas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('usuarios') ?>">Usuarios</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
 </head>
 <body>
     
